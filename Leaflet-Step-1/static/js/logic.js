d9b329958d52e4468b27df142a2c32887d6efa43
John Glen <jglendening11@gmail.com>
2021-05-18 19:21:03 -0500

    /\

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -2,9 +2,12 @@ var myMap = L.map("mapid", {
     center: [37.09, -95.71],
     zoom: 3
 });
+
 L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "light-v10",
     accessToken: API_KEY
 }).addTo(myMap);
+
+var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
